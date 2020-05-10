c2e110ff9286df86d2309c6d797d7155ce101e84
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-10 23:56:51 +0300

    function for server socket init

--- src/worker.py
+++ src/worker.py
@@ -1,16 +1,18 @@
 import socket
+
 from main_loop import MainLoop
 
-BACKLOG = 100
 
-serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def init_serversocket(addr, port, backlog):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((addr, port))
+    sock.listen(backlog)
+    sock.setblocking(False)
 
-serversock.bind(('', 8080))
-
-serversock.listen(BACKLOG)
-serversock.setblocking(False)
+    return sock
 
 
+serversock = init_serversocket('', 8080, 100)
 loop = MainLoop(serversock)
 loop.run()
