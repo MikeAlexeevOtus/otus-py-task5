3a64e7dda358fd90e0661935774b22031a94d84e
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-11 18:17:13 +0300

    refactored headers generation

--- src/response_buffer.py
+++ src/response_buffer.py
@@ -1,14 +1,25 @@
 import os
 import io
+from email.utils import formatdate
 
 
 class ResponseBuffer(object):
     FILE_READ_BLOCK = 4096
     INDEX_FILE = 'index.html'
+    CONTENT_TYPES = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'application/javascript',
+        '.jpg': 'image/jpeg',
+        '.png': 'image/png',
+        '.gif': 'image/gif',
+        '.swf': 'application/x-shockwave-flash',
+    }
+    DEFAULT_CONTENT_TYPE = 'text/plain'
 
     def __init__(self, request, document_root):
         self._request = request
-        self._root = document_root
+        self._document_root = document_root
         self._file = None
         self._file_pos = None
         self._file_end = None
@@ -16,13 +27,13 @@ class ResponseBuffer(object):
     def get_next_chunk(self):
         print('in get next chunk')
         if not os.path.exists(self._make_filepath()):
-            return self._make_headers_404()
+            return self._format_headers(self._make_headers_404())
 
         chunk = bytearray()
         if not self._file:
             # not data sent yet
             self._open_response_file()
-            chunk += self._make_headers_200()
+            chunk += self._format_headers(self._make_headers_200())
 
         chunk += self._file.read(self.FILE_READ_BLOCK)
         self._file_pos = self._file.tell()
@@ -32,7 +43,7 @@ class ResponseBuffer(object):
         return not self._file or self._file_pos != self._file_end
 
     def _make_filepath(self):
-        filepath = os.path.join(self._root, self._request.url.lstrip('/'))
+        filepath = os.path.join(self._document_root, self._request.url.lstrip('/'))
         if os.path.isdir(filepath):
             filepath = os.path.join(filepath, self.INDEX_FILE)
 
@@ -44,11 +55,30 @@ class ResponseBuffer(object):
         self._file_end = self._file.tell()
         self._file.seek(0, io.SEEK_SET)
 
-    def _make_headers_200(self):
-        resp_headers = b"""HTTP/1.0 200 OK
-Date: Mon, 1 Jan 1996 01:01:01 GMT
-Content-Type: text/plain
-Content-Length: %d
+    def _format_headers(self, headers_list):
+        return ("\r\n".join(headers_list) + "\r\n\r\n").encode()
 
-""".replace(b'\n', b'\r\n')
-        return resp_headers % self._file_end
+    def _make_headers_base(self):
+        return [
+            "Date: %s" % formatdate(timeval=None, localtime=False, usegmt=True),
+            "Server: otus-httpd",
+            "Connection: close",
+        ]
+
+    def _make_headers_200(self):
+        return [
+            "HTTP/1.0 200 OK",
+            "Content-Type: %s" % self._get_content_type(),
+            "Content-Length: %d" % self._file_end
+        ] + self._make_headers_base()
+
+    def _make_headers_404(self):
+        return [
+            "HTTP/1.0 404 Not Found",
+            "Content-Type: text/plain",
+            "Content-Length: 0"
+        ] + self._make_headers_base()
+
+    def _get_content_type(self):
+        _, ext = os.path.splitext(self._file.name)
+        return self.CONTENT_TYPES.get(ext, self.DEFAULT_CONTENT_TYPE)
