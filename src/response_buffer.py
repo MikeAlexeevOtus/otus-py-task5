c56a440cc76e929eda7150d6f4325f2977d728ca
Mikhail Alexeev <mikhail.alexeev@kaspersky.com>
2020-05-11 15:16:41 +0300

    handle index.html file

--- src/response_buffer.py
+++ src/response_buffer.py
@@ -1,16 +1,10 @@
 import os
 import io
 
-resp_headers = b"""HTTP/1.0 200 OK
-Date: Mon, 1 Jan 1996 01:01:01 GMT
-Content-Type: text/plain
-Content-Length: %d
-
-""".replace(b'\n', b'\r\n')
-
 
 class ResponseBuffer(object):
     FILE_READ_BLOCK = 4096
+    INDEX_FILE = 'index.html'
 
     def __init__(self, request, document_root):
         self._request = request
@@ -21,11 +15,14 @@ class ResponseBuffer(object):
 
     def get_next_chunk(self):
         print('in get next chunk')
+        if not os.path.exists(self._make_filepath()):
+            return self._make_headers_404()
+
         chunk = bytearray()
         if not self._file:
             # not data sent yet
             self._open_response_file()
-            chunk = resp_headers % self._file_end
+            chunk += self._make_headers_200()
 
         chunk += self._file.read(self.FILE_READ_BLOCK)
         self._file_pos = self._file.tell()
@@ -35,10 +32,23 @@ class ResponseBuffer(object):
         return not self._file or self._file_pos != self._file_end
 
     def _make_filepath(self):
-        return os.path.join(self._root, self._request.url.lstrip('/'))
+        filepath = os.path.join(self._root, self._request.url.lstrip('/'))
+        if os.path.isdir(filepath):
+            filepath = os.path.join(filepath, self.INDEX_FILE)
+
+        return filepath
 
     def _open_response_file(self):
         self._file = open(self._make_filepath(), 'rb')
         self._file.seek(0, io.SEEK_END)
         self._file_end = self._file.tell()
         self._file.seek(0, io.SEEK_SET)
+
+    def _make_headers_200(self):
+        resp_headers = b"""HTTP/1.0 200 OK
+Date: Mon, 1 Jan 1996 01:01:01 GMT
+Content-Type: text/plain
+Content-Length: %d
+
+""".replace(b'\n', b'\r\n')
+        return resp_headers % self._file_end
